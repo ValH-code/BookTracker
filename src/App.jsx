0c3bfac6c113cede64b830e8e0340e08a4fdce5e
Valentin HAMON <pro.valentin.hamon@gmail.com>
2024-12-26 19:14:25 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import AddBook from './pages/AddBook';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/add-book" element={<AddBook />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
